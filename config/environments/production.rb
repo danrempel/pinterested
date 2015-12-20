529c51fce57c7f597522cc03ae832408603b905c
danrempel <dan.rempel0@gmail.com>
2015-12-19 20:08:51 -0700

    adding international settings for aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,13 +80,14 @@ Rails.application.configure do
   #added as per Devise installation instructions
   config.action_mailer.default_url_options = { host: 'pinterested97.herokuapp.com' }
 
+  # config/environments/production.rb
   config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
+}
 
 end
---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,4 @@
+
+# config/initializers/paperclip.rb
+	Paperclip::Attachment.default_options[:url] = ':pinterested97.s3.amazonaws.com'
+	Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
