cf07b4dbf98f56a7f7d5be9f084e7e875d0e6b8c
danrempel <dan.rempel0@gmail.com>
2015-12-19 20:47:40 -0700

    added username to database and pins views

--- config/initializers/paperclip.rb
+++
@@ -1,4 +0,0 @@
-
-# config/initializers/paperclip.rb
-	Paperclip::Attachment.default_options[:url] = ':pinterested97.s3.amazonaws.com'
-	Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
